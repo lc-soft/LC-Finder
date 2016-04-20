0b2d66d05af87a8b3e43043ece2bd7503294eecd
Liu Chao <lc-soft@live.cn>
2016-04-20 21:52:07 +0800

    完善缩略图视图部件，整合滚动加载功能的实现代码

--- src/ui/thumbview.c
+++ src/ui/thumbview.c
@@ -1,22 +1,66 @@
-﻿
+﻿/* ***************************************************************************
+* thumbview.c -- thumbnail list view
+*
+* Copyright (C) 2016 by Liu Chao <lc-soft@live.cn>
+*
+* This file is part of the LC-Finder project, and may only be used, modified,
+* and distributed under the terms of the GPLv2.
+*
+* By continuing to use, modify, or distribute this file you indicate that you
+* have read the license and understand and accept it fully.
+*
+* The LC-Finder project is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.
+*
+* You should have received a copy of the GPLv2 along with this file. It is
+* usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.
+* ****************************************************************************/
+
+/* ****************************************************************************
+* thumbview.c -- 缩略图列表视图部件，主要用于以缩略图形式显示文件夹和文件列表
+*
+* 版权所有 (C) 2016 归属于 刘超 <lc-soft@live.cn>
+*
+* 这个文件是 LC-Finder 项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和
+* 发布。
+*
+* 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。
+*
+* LC-Finder 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销
+* 性或特定用途的隐含担保，详情请参照GPLv2许可协议。
+*
+* 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在 LICENSE 文件中，如果
+* 没有，请查看：<http://www.gnu.org/licenses/>.
+* ****************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
-#include <LCUI_Build.h>
-#include <LCUI/LCUI.h>
+#include "finder.h"
+#include <LCUI/timer.h>
 #include <LCUI/display.h>
 #include <LCUI/gui/widget.h>
 #include <LCUI/gui/widget/textview.h>
-#include "finder.h"
 #include "thumbview.h"
 
+typedef struct ScrollLoadingRec_ {
+	int top;
+	int event_id;
+	LCUI_BOOL enabled;
+	LCUI_Widget scrolllayer;
+	LCUI_Widget top_child;
+} ScrollLoadingRec, *ScrollLoading;
+
 typedef struct ThumbViewRec_ {
+	Dict *dbs;
 	ThumbCache cache;
-	LCUI_BOOL is_loading;
+	LinkedList tasks;
 	LCUI_Cond cond;
 	LCUI_Mutex mutex;
 	LCUI_Thread thread;
-	LinkedList tasks;
-	Dict *dbs;
+	LCUI_BOOL is_loading;
+	LCUI_BOOL need_update;
+	ScrollLoading scrollload;
 } ThumbViewRec, *ThumbView;
 
 typedef struct ThumbFileInfoRec_ {
@@ -35,8 +79,100 @@ typedef struct ThumbItemDataRec_ {
 } ThumbItemDataRec, *ThumbItemData;
 
 #define THUMB_CACHE_SIZE (20*1024*1024)
-
 #define THUMB_MAX_WIDTH 240
+
+static int scrollload_event = -1;
+
+static int ScrollLoading_Update( ScrollLoading ctx )
+{
+	LCUI_Widget w;
+	LinkedListNode *node;
+	LCUI_WidgetEventRec e;
+	int count = 0, top, bottom;
+
+	if( !ctx->enabled ) {
+		return 0;
+	}
+	e.type = ctx->event_id;
+	e.cancel_bubble = TRUE;
+	bottom = top = ctx->top;
+	bottom += ctx->scrolllayer->parent->box.padding.height;
+	if( !ctx->top_child ) {
+		node = ctx->scrolllayer->children.head.next;
+		if( node ) {
+			ctx->top_child = node->data;
+		}
+	}
+	if( !ctx->top_child ) {
+		return 0;
+	}
+	if( ctx->top_child->box.border.top > bottom ) {
+		node = Widget_GetNode( ctx->top_child );
+		ctx->top_child = NULL;
+		while( node ) {
+			w = node->data;
+			if( w->box.border.y < bottom ) {
+				ctx->top_child = w;
+				break;
+			}
+			node = node->prev;
+		}
+		if( !ctx->top_child ) {
+			return 0;
+		}
+	}
+	node = Widget_GetNode( ctx->top_child );
+	while( node ) {
+		w = node->data;
+		if( w->box.border.y > bottom ) {
+			break;
+		}
+		if( w->box.border.y + w->box.border.height >= top ) {
+			Widget_TriggerEvent( w, &e, NULL );
+			++count;
+		}
+		node = node->next;
+	}
+	return count;
+}
+
+static void OnScroll( LCUI_Widget w, LCUI_WidgetEvent e, void *arg )
+{
+	int *scroll_pos = arg;
+	ScrollLoading ctx = e->data;
+	ctx->top = *scroll_pos;
+	ScrollLoading_Update( ctx );
+}
+
+ScrollLoading ScrollLoading_New( LCUI_Widget scrolllayer )
+{
+	ScrollLoading ctx = NEW( ScrollLoadingRec, 1 );
+	ctx->top = 0;
+	ctx->top_child = NULL;
+	ctx->event_id = scrollload_event;
+	ctx->scrolllayer = scrolllayer;
+	Widget_BindEvent( scrolllayer, "scroll", OnScroll, ctx, NULL );
+	return ctx;
+}
+
+static void ScrollLoading_Delete( ScrollLoading ctx )
+{
+	ctx->top = 0;
+	ctx->top_child = NULL;
+	Widget_UnbindEvent( ctx->scrolllayer, "scroll", OnScroll );
+	free( ctx );
+}
+
+static void ScrollLoading_Reset( ScrollLoading ctx )
+{
+	ctx->top = 0;
+	ctx->top_child = NULL;
+}
+
+static void ScrollLoading_Enable( ScrollLoading ctx, LCUI_BOOL enable )
+{
+	ctx->enabled = enable;
+}
 
 static int GetDirThumbFilePath( char *filepath, char *dirpath )
 {
@@ -144,6 +280,7 @@ static void ThumbView_TaskThread( void *arg )
 	while( 1 ) {
 		LCUICond_Wait( &view->cond, &view->mutex );
 		view->is_loading = TRUE;
+		_DEBUG_MSG("[%p] loading thumb...\n", view);
 		LinkedList_ForEach( node, &view->tasks ) {
 			prev = node->prev;
 			LinkedList_Unlink( &view->tasks, node );
@@ -169,6 +306,18 @@ static void ThumbView_TaskThread( void *arg )
 	}
 }
 
+void ThumbView_Lock( LCUI_Widget w )
+{
+	ThumbView view = w->private_data;
+	ScrollLoading_Enable( view->scrollload, FALSE );
+}
+
+void ThumbView_Unlock( LCUI_Widget w )
+{
+	ThumbView view = w->private_data;
+	ScrollLoading_Enable( view->scrollload, TRUE );
+}
+
 void ThumbView_Reset( LCUI_Widget w )
 {
 	ThumbView view;
@@ -177,6 +326,7 @@ void ThumbView_Reset( LCUI_Widget w )
 	LCUIMutex_Lock( &view->mutex );
 	LinkedList_Clear( &view->tasks, free );
 	LCUIMutex_Unlock( &view->mutex );
+	ScrollLoading_Reset( view->scrollload );
 }
 
 static void OnScrollLoad( LCUI_Widget w, LCUI_WidgetEvent e, void *arg )
@@ -191,6 +341,18 @@ static void OnScrollLoad( LCUI_Widget w, LCUI_WidgetEvent e, void *arg )
 	task->info = &data->info;
 	LinkedList_Append( &data->view->tasks, task );
 	LCUICond_Signal( &data->view->cond );
+	_DEBUG_MSG("on scroll load: %s\n", task->info->path);
+}
+
+static void UpdateView( void *arg )
+{
+	LCUI_Widget w = arg;
+	ThumbView view = w->private_data;
+	if( !view->need_update ) {
+		return;
+	}
+	view->need_update = FALSE;
+	ScrollLoading_Update( view->scrollload );
 }
 
 LCUI_Widget ThumbView_AppendFolder( LCUI_Widget w, const char *filepath, 
@@ -224,6 +386,8 @@ LCUI_Widget ThumbView_AppendFolder( LCUI_Widget w, const char *filepath,
 	Widget_Append( infobar, icon );
 	Widget_BindEvent( item, "scrollload", OnScrollLoad, NULL, NULL );
 	Widget_Append( w, item );
+	data->view->need_update = TRUE;
+	LCUITimer_Set( 200, UpdateView, w, FALSE );
 	return item;
 }
 
@@ -240,6 +404,8 @@ LCUI_Widget ThumbView_AppendPicture( LCUI_Widget w, const char *path )
 	Widget_AddClass( item, "file-list-item-picture" );
 	Widget_BindEvent( item, "scrollload", OnScrollLoad, NULL, NULL );
 	Widget_Append( w, item );
+	data->view->need_update = TRUE;
+	LCUITimer_Set( 200, UpdateView, w, FALSE );
 	return item;
 }
 
@@ -248,17 +414,20 @@ static void ThumbView_OnInit( LCUI_Widget w )
 	ThumbView self;
 	self = Widget_NewPrivateData( w, ThumbViewRec );
 	self->is_loading = FALSE;
+	self->need_update = FALSE;
 	self->dbs = finder.thumb_dbs;
 	LCUICond_Init( &self->cond );
 	LCUIMutex_Init( &self->mutex );
 	LinkedList_Init( &self->tasks );
+	self->scrollload = ScrollLoading_New( w );
 	self->cache = ThumbCache_New( THUMB_CACHE_SIZE, OnRemoveThumb );
 	LCUIThread_Create( &self->thread, ThumbView_TaskThread, self );
 }
 
 static void ThumbView_OnDestroy( LCUI_Widget w )
 {
-
+	ThumbView self = w->private_data;
+	ScrollLoading_Delete( self->scrollload );
 }
 
 void LCUIWidget_AddTThumbView( void )
@@ -266,4 +435,6 @@ void LCUIWidget_AddTThumbView( void )
 	LCUI_WidgetClass *wc = LCUIWidget_AddClass( "thumbview" );
 	wc->methods.init = ThumbView_OnInit;
 	wc->methods.destroy = ThumbView_OnDestroy;
+	scrollload_event = LCUIWidget_AllocEventId();
+	LCUIWidget_SetEventName( scrollload_event, "scrollload" );
 }
