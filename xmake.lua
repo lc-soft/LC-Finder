b66460455e244b7e50d494cd726b828af1b0b314
Liu Chao <lc-soft@live.cn>
2016-09-28 00:19:26 +0800

    修改构建配置和脚本

--- xmake.lua
+++ xmake.lua
@@ -4,8 +4,9 @@ set_warnings("all")
 set_config_h("include/config.h")
 add_cfuncs("3rdparty", "sqlite3", "sqlite3.h", "sqlite3_open")
 add_includedirs("include", ".repos/LCUI/include")
+add_ldflags(".repos/LCUI/src/.libs/libLCUI.a")
 add_ldflags("-L.repos/LCUI/src/.libs")
-add_ldflags("-lLCUI")
+add_ldflags("$(shell pkg-config .repos/LCUI/lcui.pc --static --libs-only-l)")
 
 if is_mode("debug") then
     set_symbols("debug")
