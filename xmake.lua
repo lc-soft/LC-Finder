da2e33694f5c7c68e072d12aec5dd56178d5de1b
Liu Chao <lc-soft@live.cn>
2018-07-28 17:23:47 +0800

    chore(scripts): update xmake config

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,15 @@
-set_project("lcfinder")
+set_project("lc-finder")
 set_version("0.11.0")
 set_warnings("all")
-set_config_h("include/config.h")
+set_config_header("include/config.h", {prefix = "LCFINDER"})
 add_cfuncs("3rdparty", "sqlite3", "sqlite3.h", "sqlite3_open")
-add_includedirs("include", ".repos/LCUI/include", ".repos/libyaml/include")
+add_cflags("$(shell pkg-config .repos/LCUI/lcui.pc --static --cflags-only-other)")
+add_ldflags("-L.repos/LCUI/src/.libs")
+add_ldflags("-L.repos/libyaml/src/.libs")
+add_ldflags(".repos/LCUI/src/.libs/libLCUI.a -lpthread")
 add_ldflags("$(shell pkg-config .repos/LCUI/lcui.pc --static --libs-only-l)")
 add_ldflags("$(shell pkg-config .repos/libyaml/yaml-0.1.pc --static --libs-only-l)")
-
+add_includedirs("include", ".repos/LCUI/include", ".repos/libyaml/include")
 if is_mode("debug") then
     set_symbols("debug")
     set_optimize("none")
@@ -17,7 +20,7 @@ if is_mode("release") then
     set_strip("all")
 end
 
-target("lcfinder")
+target("lc-finder")
+    set_targetdir("app/")
     set_kind("binary")
     add_files("src/**.c")
-
